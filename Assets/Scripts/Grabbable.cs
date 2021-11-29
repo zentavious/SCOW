46262aba5125ba361c5b04055c7f8b0c9e36d1d4
Matthew J Zent <zentx005@umn.edu>
2021-11-29 16:19:28 -0600

    added select functionality

--- Assets/Scripts/Grabbable.cs
+++ Assets/Scripts/Grabbable.cs
@@ -5,47 +5,59 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
+
     public float effectRadius; // needs to be smaller than collider radius
     public float viscocity; // propotion of effectRadius to use a movement inside collider
 
     private Vector3 originalPosition;
-    private GameObject controller;
+    private Grabber controller;
     private bool isHighlighted;
+    private bool isGrabbed;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        isHighlighted = false;
+        this.isHighlighted = false;
+        this.isGrabbed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (controller)
+        if (!isGrabbed)
         {
-            if (!isHighlighted)
+            if (controller?.IsEffectOn() ?? false)
             {
-                if (Vector3.Distance(this.originalPosition, this.controller.transform.position) <= effectRadius)
+                if (!isHighlighted)
                 {
-                    Guid x = Guid.NewGuid();
-                    var distanceFromController = Vector3.Distance(this.originalPosition, this.controller.transform.position);
-                    var newDistance = distanceFromController / 2f - effectRadius / 2f; // TODO: needs to factor in size of selected object
-                    Debug.Log($"{x} - dist: {newDistance}");
-                    var newPos = Vector3.MoveTowards(this.originalPosition, this.controller.transform.position, newDistance);
-                    newPos = Vector3.MoveTowards(this.transform.position, newPos, this.viscocity * this.effectRadius);
-                    Debug.Log($"{x} - newPos: {newPos}");
-                    this.transform.position = newPos;
+                    if (Vector3.Distance(this.originalPosition, this.controller.transform.position) <= effectRadius)
+                    {
+                        Guid x = Guid.NewGuid();
+                        var distanceFromController = Vector3.Distance(this.originalPosition, this.controller.transform.position);
+                        var newDistance = distanceFromController / 2f - effectRadius / 2f; // TODO: needs to factor in size of selected object
+                        var newPos = Vector3.MoveTowards(this.originalPosition, this.controller.transform.position, newDistance);
+                        newPos = Vector3.MoveTowards(this.transform.position, newPos, this.viscocity * this.effectRadius);
+                        this.transform.position = newPos;
+                    }
+                    else
+                    {
+                        this.transform.position = Vector3.MoveTowards(this.transform.position, this.originalPosition, this.viscocity * this.effectRadius);
+                    }
                 }
                 else
                 {
-                    //this.transform.position = this.originalPosition;
-                    this.transform.position = Vector3.MoveTowards(this.transform.position, this.originalPosition, this.viscocity * this.effectRadius);
+                    this.transform.position = Vector3.MoveTowards(this.transform.position, this.controller.transform.position, this.viscocity * this.effectRadius);
                 }
             }
-            else
+            else if (!controller?.IsEffectOn() ?? false)
             {
-                this.transform.position = Vector3.MoveTowards(this.transform.position, this.controller.transform.position, this.viscocity * this.effectRadius);
+                this.transform.position = Vector3.MoveTowards(this.transform.position, this.originalPosition, this.viscocity * this.effectRadius);
             }
+        }
+        else
+        {
+            this.originalPosition = this.transform.position;
         }
     }
 
@@ -54,7 +66,7 @@ public class Grabbable : MonoBehaviour
         if (other.GetComponent<Grabber>())
         {
             this.originalPosition = this.transform.position;
-            this.controller = other.gameObject;
+            this.controller = other.GetComponent<Grabber>();
         }
     }
 
@@ -62,7 +74,7 @@ public class Grabbable : MonoBehaviour
     {
         if (other.GetComponent<Grabber>())
         {
-            this.transform.position = this.originalPosition;
+            this.transform.position = this.originalPosition; // might not be needed
             this.controller = null;
         }
     }
@@ -75,8 +87,36 @@ public class Grabbable : MonoBehaviour
     {
         this.isHighlighted = false;
     }
+
+    public bool IsHighlighted()
+    {
+        return this.isHighlighted;
+    }
     public Vector3 GetOriginalPosition()
     {
         return this.originalPosition;
+    }
+
+    public void Grab(Transform parent)
+    {
+        this.isGrabbed = true;
+
+        this.transform.position = parent.position;
+        this.transform.parent = parent;
+    }
+
+    public void Release()
+    {
+        this.originalPosition = this.transform.position;
+        this.transform.parent = null;
+
+        this.isGrabbed = false;
+    }
+
+    public bool Select()
+    {
+        var outline = this.GetComponent<Outline>();
+        outline.enabled = !outline.enabled;
+        return outline.enabled;
     }
 }
--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -1,43 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Grabber : MonoBehaviour
 {
     public float effectRadius; // needs to be smaller than collider radius
+    public InputActionProperty grabAction;
+    public InputActionProperty selectAction;
 
     private List<Grabbable> Grabbables = new List<Grabbable>();
+    private GameObject grabbedObject;
+    private GameObject selectedObject;
+    private bool effectOn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        grabbedObject = null;
+        effectOn = true;
+
+        grabAction.action.performed += Grab;
+        grabAction.action.canceled += Release;
+
+
+        selectAction.action.performed += Select;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Grabbable closestGrabbable = null;
-        var closestDistance = 0f;
-        foreach (var grabbable in Grabbables)
-        {
-            grabbable.Unhighlight();
-            var distance = Vector3.Distance(grabbable.GetOriginalPosition(), this.gameObject.transform.position);
-            if (!closestGrabbable)
-            {
-                closestGrabbable = grabbable;
-                closestDistance = distance;
-            }
-            else if (closestDistance > distance)
-            {
-                closestDistance = distance;
-                closestGrabbable = grabbable;
-            }
-        }
-        if (closestDistance <= this.effectRadius)
-        {
-            closestGrabbable?.Highlight();
-        }
+        this.GetClosestGrabbable(highlight: true);
     }
 
     void OnTriggerEnter(Collider other)
@@ -56,5 +49,90 @@ public class Grabber : MonoBehaviour
         {
             this.Grabbables.Remove(grabbable);
         }
+    }
+        
+    public void Grab(InputAction.CallbackContext context)
+    {
+        if (this.grabbedObject == null)
+        {
+            this.grabbedObject = GetClosestGrabbable();
+            var grabbable = this.grabbedObject.GetComponent<Grabbable>();
+
+            grabbable.Grab(parent: this.transform);
+            this.effectOn = false;
+        }
+    }
+
+    public void Release(InputAction.CallbackContext context)
+    {
+        if (this.grabbedObject)
+        {
+            var grabbable = this.grabbedObject.GetComponent<Grabbable>();
+
+            grabbable.Release();
+            this.grabbedObject = null;
+            this.effectOn = this.selectedObject == null; // TODO: effect should only be on after sphere cast, work around bc cast is not implimented
+        }
+    }
+
+
+    public void Select(InputAction.CallbackContext context)
+    {
+        if (this.selectedObject == null)
+        {
+            this.selectedObject = GetClosestGrabbable();
+
+            var grabbable = this.selectedObject.GetComponent<Grabbable>(); // Will null ref is select is pressed without an object
+            grabbable.Select();
+
+            this.effectOn = false;
+        }
+        else
+        {
+            var grabbable = this.selectedObject.GetComponent<Grabbable>();
+            
+            grabbable.Select();
+
+            this.selectedObject = null;
+            this.effectOn = this.grabbedObject == null; // TODO: effect should only be on after sphere cast, work around bc cast is not implimented
+        }
+    }
+
+    public bool IsEffectOn()
+    {
+        return this.effectOn;
+    }
+
+    private GameObject GetClosestGrabbable(bool highlight = false)
+    {
+        Grabbable closestGrabbable = null;
+        var closestDistance = 0f;
+
+        foreach (var grabbable in this.Grabbables)
+        {
+            grabbable.Unhighlight();
+            var distance = Vector3.Distance(grabbable.GetOriginalPosition(), this.gameObject.transform.position);
+            if (!closestGrabbable)
+            {
+                closestGrabbable = grabbable;
+                closestDistance = distance;
+            }
+            else if (closestDistance > distance)
+            {
+                closestDistance = distance;
+                closestGrabbable = grabbable;
+            }
+        }
+
+        if (closestDistance <= this.effectRadius)
+        {
+            if (highlight)
+            {
+                closestGrabbable?.Highlight();
+            }
+            return closestGrabbable?.gameObject;
+        }
+
+        return null;
     }
 }
