eda1e3f302f3526f4496619a1abdf28886eee0a9
Matthew J Zent <zentx005@umn.edu>
2021-12-08 14:27:24 -0600

    scene changes for parent child relationship of cubes

--- Assets/Scripts/WIM.cs
+++ Assets/Scripts/WIM.cs
@@ -13,6 +13,9 @@ public class WIM : MonoBehaviour
         // this.parentWIMObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + .05f, this.transform.position.z);
         this.lastPos = this.transform.position;
         this.miniObjects = new List<GameObject>();
+
+        this.CastObjectsToWIM(parentWIMObject); // cheat until we actually start casting
+        /*
         int i = 0;
         foreach (Transform child in this.parentWIMObject.transform) // TODO: we cheat here by getting the relative position of every object here at the start. This will need to be done later on cast
         {
@@ -23,7 +26,7 @@ public class WIM : MonoBehaviour
             gameObject.transform.parent = this.transform;
             i++;
         }
-        Debug.Log($"There are {i} children");
+        Debug.Log($"There are {i} children");*/
     }
 
     // Update is called once per frame
@@ -53,6 +56,28 @@ public class WIM : MonoBehaviour
         }
 
         this.lastPos = this.transform.position;
+    }
+
+    public void CastObjectsToWIM(GameObject parentWIMObject)
+    {
+        int i = 0;
+        this.parentWIMObject = new GameObject("Parent Container");
+        this.parentWIMObject.transform.position = this.transform.position;
+        foreach (Transform child in parentWIMObject.transform)
+        {
+            var clone = GameObject.Instantiate(child.gameObject);
+            clone.transform.parent = this.parentWIMObject.transform;
+            Debug.Log(child.transform.localPosition);
+            clone.transform.localPosition = child.transform.localPosition;
+
+            this.miniObjects.Add(clone);
+
+            var gameObject = new GameObject($"Object_{i}");
+            gameObject.transform.position = child.gameObject.transform.position;
+            gameObject.transform.parent = this.transform;
+
+            i++;
+        }
     }
 
 }
